eb098fa73788c228da4fd3492a7cbc8e1244c66d
jb-holographik <jean-baptiste@holographik.co>
2026-05-12 13:13:08 +0200

    Update Vite configuration to conditionally set asset base path for production and disable polyfill module preload, while updating chunk references in the manifest for optimized asset loading.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
 
 // vite.config.js
+const PRODUCTION_ASSET_BASE =
+  process.env.VITE_ASSET_BASE || 'https://precious-hotteok-8da21f.netlify.app/'
+
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? PRODUCTION_ASSET_BASE : './',
   plugins: [eslintPlugin({ cache: false })],
   server: {
     host: 'localhost',
@@ -16,6 +19,7 @@ export default defineConfig({
   build: {
     minify: true,
     manifest: true,
+    polyfillModulePreload: false,
     rollupOptions: {
       input: './src/main.js',
       output: {
